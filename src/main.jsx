95ee31cac60d9649589f05ea152deccf7d3d5623
Orlandoseoanes <oseoanesdeveloper@gmail.com>
2023-07-06 19:16:47 -0500

    Rutas principales v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import Menu from './vistas/menu'
+import Producto from './vistas/Producto'
+import ProductoIndividual from './vistas/productoindividual'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Menu />,
+    errorElement: <h1>404</h1>,
+  },
+  {
+    path: '/listaproductos',
+    element: <Producto />,
+  },
+  {
+    path: '/productoindividual',
+    element: <ProductoIndividual />,
+  },
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <RouterProvider router={router}/>
+   </React.StrictMode>,
 )
